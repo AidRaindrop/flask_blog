b494296d915e7253c073a51d0a37fffd8f5a806b
eduard <burak.edouard@yandex.by>
2023-06-16 16:18:31 +0300

    itproger_flask_lesson3_4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,7 +9,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 
-
+with app.app_context():
+    db.create_all()
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -32,6 +33,34 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+@app.route('/create-article', methods = ('POST', 'GET'))
+def create_article():
+    if request.method == "POST":
+        title = request.form["title"]
+        intro = request.form["intro"]
+        text = request.form["text"]
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except Exception as e:
+            return str(e)
+            #return "when add article create error"
+    else:
+        return render_template("create-article.html")
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
